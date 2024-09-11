b72446bf252cf2535ed119ae851ffc11400d885a
raspberry <shriyashdixit@gmail.com>
2024-09-11 10:42:06 +0530

    added fields for algo trading

--- django_app/algo_trading/admin.py
+++ django_app/algo_trading/admin.py
@@ -7,4 +7,4 @@ from .models import HistoricalData
 @admin.register(HistoricalData)
 class HistoricalDataAdmin(admin.ModelAdmin):
     search_fields = ['ticker']
-    list_display = ['ticker', 'date', 'open', 'high', 'low', 'close']
+    list_display = ['ticker', 'date', 'open', 'high', 'low', 'close', 'total_traded_quantity']
