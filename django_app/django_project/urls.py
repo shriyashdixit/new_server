28af97f970014341d63618f267c44858e77af5c6
Shriyash <shriyashd@gmail.com>
2025-08-02 13:35:22 +0530

    deployed nextcloud

--- django_app/django_project/urls.py
+++ django_app/django_project/urls.py
@@ -46,5 +46,8 @@ urlpatterns = [
     path('send-message/', send_message_view, name='send_message'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
